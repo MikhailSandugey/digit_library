c63deecc0729c0732846398a075f770785a7d898
MikhailSandugey <99660941+MikhailSandugey@users.noreply.github.com>
2022-11-13 19:52:16 +0300

    added logout function, minor fixes

--- src/main/java/com/example/digitlib/config/SecurityConfig.java
+++ src/main/java/com/example/digitlib/config/SecurityConfig.java
@@ -34,7 +34,11 @@ public class SecurityConfig {
                 .loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/", true)
-                .failureUrl("/auth/login?error");
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
         return http.build();
     }
 
