e1f47dcd2dc5bb15ee80cbf04b5d1ead01345633
MikhailSandugey <99660941+MikhailSandugey@users.noreply.github.com>
2022-11-13 20:08:19 +0300

    added encoding for passwords

--- src/main/java/com/example/digitlib/config/SecurityConfig.java
+++ src/main/java/com/example/digitlib/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -44,7 +44,9 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
+
+
 }
 
--- src/main/java/com/example/digitlib/services/RegistrationService.java
+++ src/main/java/com/example/digitlib/services/RegistrationService.java
@@ -3,6 +3,7 @@ package com.example.digitlib.services;
 import com.example.digitlib.models.Admin;
 import com.example.digitlib.repositories.AdminsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,14 +12,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final AdminsRepository adminsRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(AdminsRepository adminsRepository) {
+    public RegistrationService(AdminsRepository adminsRepository, PasswordEncoder passwordEncoder) {
         this.adminsRepository = adminsRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void registerAdmin(Admin admin) {
+        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
         adminsRepository.save(admin);
     }
 }
